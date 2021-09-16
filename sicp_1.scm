4ce66386a856965130adca2e1bc0c65323559abb
endlmk <1269467+endlmk@users.noreply.github.com>
2021-09-16 23:35:29 +0900

    Done ex1.35.

--- sicp_1.scm
+++ sicp_1.scm
@@ -481,3 +481,18 @@
 (f (lambda (g) (g 2)))
 ((lambda (g) (g 2)) 2)
 (2 2)
+
+;;ex1-35
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+	  next
+	  (try next))))
+  (try first-guess))
+(define (golden-ratio)
+  (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
