dfa90357a17343234f546838268be10a603e9684
endlmk <1269467+endlmk@users.noreply.github.com>
2021-11-28 22:25:20 +0900

    Done 2.46 to 2.51.

--- sicp_2.scm
+++ sicp_2.scm
@@ -491,3 +491,186 @@
       painter
       (let ((smaller (split io so (- n 1))))
 	(io painter (so smaller smaller)))))
+
+;;ex2-46
+(define (make-vect x y)
+  (cons x y))
+(define (xcor-vect v)
+  (car v))
+(define (ycor-vect v)
+  (cdr v))
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2)) (+ (ycor-vect v1) (ycor-vect v2))))
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2)) (- (ycor-vect v1) (ycor-vect v2))))
+(define (scale-vect s v1)
+  (make-vect (* s (xcor-vect v1)) (* s (ycor-vect v1))))
+
+;;ex2-47
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect (scale-vect (xcor-vect v) (edge1-frame frame))
+	       (scale-vect (ycor-vect v) (edge2-frame frame))))))
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+(define (origin-frame f)
+  (car f))
+(define (edge1-frame f)
+  (cadr f))
+(define (edge2-frame f)
+  (caddr f))
+
+(define (maek-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+(define (origin-frame f)
+  (car f))
+(define (edge1-frame f)
+  (cadr f))
+(define (edge2-frame f)
+  (cddr f))
+
+;;ex2-48
+(define (make-segment s e)
+  (cons s e))
+(define (start-segment seg)
+  (car seg))
+(define (end-segment seg)
+  (cdr seg))
+
+;;ex2-49
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (segment)
+       (draw-line
+	((frame-coord-map frame)
+	 (start-segment segment))
+	((frame-coord-map frame)
+	 (end-segment segment))))
+     (segment-list))))
+;;a
+(define edge-painter
+  (segments->painter
+   (list (make-segment (make-vect 0 0) (make-vect 1 0))
+	 (make-segment (make-vect 0 0) (make-vect 0 1))
+	 (make-segment (make-vect 1 0) (make-vect 1 1))
+	 (make-segment (make-vect 0 1) (make-vect 1 1)))))
+;;b
+(define diag-frame-painter
+  (segments->painter
+   (list (make-segment (make-vect 1 0) (make-vect 0 1))
+	 (make-segment (make-vect 0 0) (make-vect 1 1)))))
+;;c
+(define mid-rhombus-painter
+  (segments->painter
+   (list (make-segment (make-vect 0.5 0) (make-vect 1 0.5))
+	 (make-segment (make-vect 1 0.5) (make-vect 0.5 1))
+	 (make-segment (make-vect 0.5 1) (make-vect 0 0.5))
+	 (make-segment (make-vect 0 0.5) (make-vect 0.5 0)))))
+;;d
+(define wave
+  (segments->painter
+   (list (make-segment (make-vect 0.35 0.85) (make-vect 0.40 1.00))
+         (make-segment (make-vect 0.65 0.85) (make-vect 0.60 1.00))
+         (make-segment (make-vect 0.35 0.85) (make-vect 0.40 0.65))
+         (make-segment (make-vect 0.65 0.85) (make-vect 0.60 0.65))
+         (make-segment (make-vect 0.60 0.65) (make-vect 0.75 0.65))
+         (make-segment (make-vect 0.40 0.65) (make-vect 0.30 0.65))
+         (make-segment (make-vect 0.75 0.65) (make-vect 1.00 0.35))
+         (make-segment (make-vect 0.60 0.45) (make-vect 1.00 0.15))
+         (make-segment (make-vect 0.60 0.45) (make-vect 0.75 0.00))
+         (make-segment (make-vect 0.50 0.30) (make-vect 0.60 0.00))
+         (make-segment (make-vect 0.30 0.65) (make-vect 0.15 0.60))
+         (make-segment (make-vect 0.30 0.60) (make-vect 0.15 0.40))
+         (make-segment (make-vect 0.15 0.60) (make-vect 0.00 0.85))
+         (make-segment (make-vect 0.15 0.40) (make-vect 0.00 0.65))
+         (make-segment (make-vect 0.30 0.60) (make-vect 0.35 0.50))
+         (make-segment (make-vect 0.35 0.50) (make-vect 0.25 0.00))
+         (make-segment (make-vect 0.50 0.30) (make-vect 0.40 0.00)))))
+
+;;ex2-50
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+	(painter (make-frame
+		  new-origin
+		  (sub-vect (m corner1) new-origin)
+		  (sub-vect (m corner2) new-origin)))))))
+(define (flip-vert painter)
+  (transform-painter painter
+		     (make-vect 0.0 1.0)
+		     (make-vect 1.0 1.0)
+		     (make-vect 0.0 0.0)))
+(define (shirink-to-upper-right painter)
+  (transform-painter painter
+		     (make-vect 0.5 0.5)
+		     (make-vect 1.0 0.5)
+		     (make-vect 0.5 1.0)))
+(define (rotate90 painter)
+  (transform-painter painter
+		     (make-vect 1.0 0.0)
+		     (make-vect 1.0 1.0)
+		     (make-vect 0.0 0.0)))
+(define (squash-inwards painter)
+  (transform-painter painter
+		     (make-vect 0.0 0.0)
+		     (make-vect 0.65 0.35)
+		     (make-vect 0.35 0.65)))
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left
+	   (transform-painter painter1
+			      (make-vect 0.0 0.0)
+			      split-point
+			      (make-vect 0.0 1.0)))
+	  (paint-right
+	   (transform-painter painter2
+			      split-point
+			      (make-vect 1.0 0.0)
+			      (make-vect 0.5 1.0))))
+      (lambda (frame)
+	(paint-left frame)
+	(paint-right frame)))))
+(define (flip-horiz painter)
+  (transform-painter painter
+		     (make-vect 1.0 0.0)
+		     (make-vect 0.0 0.0)
+		     (make-vect 1.0 1.0)))
+(define (rotate-180 painter)
+  (transform-painter painter
+		     (make-vect 1.0 1.0)
+		     (make-vect 0.0 1.0)
+		     (make-vect 1.0 0.0)))
+(define (rotate-270 painter)
+  (transform-painter painter
+		     (make-vect 0.0 1.0)
+		     (make-vect 0.0 0.0)
+		     (make-vect 1.0 1.0)))
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-top
+	   (transform-painter painter1
+			      split-point
+			      (make-vect 1.0 0.5)
+			      (make-vect 0.0 1.0)))
+	  (paint-botom
+	   (transform-painter painter2
+			      (make-vect 0.0 0.0)
+			      (make-vect 1.0 0.0)
+			      split-point)))
+      (lambda (frame)
+	(paint-top frame)
+	(paint-bottom frame)))))
+(define (below painter1 painter2)
+  (rotate-270 (beside (rotate-90 painter1) (rotate-90 painter2))))
+
+
+    
+
+
+
+
+	  
