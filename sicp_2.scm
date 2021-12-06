21fe3b6e1ce8518a1d120028a23bc28e01782c9d
endlmk <1269467+endlmk@users.noreply.github.com>
2021-12-06 23:01:12 +0900

    Done 2.63.

--- sicp_2.scm
+++ sicp_2.scm
@@ -900,3 +900,42 @@
 		(cond ((= x1 x2) (cons x1 (union-set (cdr set1) (cdr set2))))
 		      ((< x1 x2) (cons x1 (cons x2 (union-set (cdr set1) (cdr set2)))))
 		      ((> x1 x2) (cons x2 (cons x1 (union-set (cdr set1) (cdr set2))))))))))
+
+;;ex2-63
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+	((= x (entry set)) #t)
+	((< x (entry set)) (element-of-set? x (left-branch set)))
+	((> x (entry set)) (element-of-set? x (right-branch set)))))
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x () ()))
+	((= x (entry set)) set)
+	((< x (entry set)) (make-tree (entry set) (adjoin-set x (left-branch set)) (right-branch set)))
+	((> x (entry set)) (make-tree (entry set) (left-branch set) (adjoin-set x (right-branch set))))))
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      ()
+      (append (tree->list-1 (left-branch tree))
+	      (cons (entry tree) (tree->list-1 (right-branch tree))))))
+;;(1 3 5 7 9 11)
+;;(1 3 5 7 9 11)
+;;(1 3 5 7 9 11)
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+	result-list
+	(copy-to-list (left-branch tree)
+		      (cons (entry tree) (copy-to-list (right-branch tree) result-list)))))
+  (copy-to-list tree ()))
+;;(1 3 5 7 9 11)
+;;(1 3 5 7 9 11)
+;;(1 3 5 7 9 11)
+
+;;ツリーの走査のオーダーは同じ。appendの分、1のほうが遅い。
