5d424b3a5b25f09257cae7e06ff1f08da69070f9
endlmk <1269467+endlmk@users.noreply.github.com>
2021-12-20 23:46:03 +0900

    Done 2.78.

--- sicp_2.scm
+++ sicp_2.scm
@@ -1305,5 +1305,18 @@
 ;;complex型の数値のタグは(complex rectangular ...)となっており、
 ;;1つ目のタグであるcomplexに対してapply-genericすることで、rectangular型またはpolar型用のmagnitudeに処理を転送するようになるため。
 
+;;ex2-78
+(define (attach-tag type-tag contents)
+  (if (eq? type-tag 'scheme-number)
+      contents
+      (cons type-tag contents)))
+(define (type-tag datum)
+  (cond ((number? datum) 'scheme-number)
+	((pair? datum) (car datum))
+	(else (error "Bad tagged datum: TYPE-TAG" datum))))
+(define (contents datum)
+  (cond ((number? datum) datum)
+	((pair? datum) (cdr datum))
+	(else (error "Bad tagged datum: CONTENTS" datum))))
 
   
