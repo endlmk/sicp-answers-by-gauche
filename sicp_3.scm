db5658bef71009660fa719187bf2de92014b5fff
endlmk <1269467+endlmk@users.noreply.github.com>
2022-01-22 22:24:28 +0900

    Done 3.24.

--- sicp_3.scm
+++ sicp_3.scm
@@ -517,3 +517,36 @@
 (print-deque d1) ;; (1)
 (rear-delete-queue! d1)
 (print-deque d1) ;; ()
+
+;;ex3.24
+(define (assoc-if key records predicate)
+  (cond ((null? records) #f)
+	((predicate key (caar records)) (car records))
+	(else (assoc-if key (cdr records) predicate))))
+
+(define (make-table same-key?)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc-if key-1 (cdr local-table) same-key?)))
+	(if subtable
+	    (let ((record (assoc-if key-2 (cdr subtable) same-key?)))
+	      (if record (cdr record) #f))
+	    #f)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc-if key-1 (cdr local-table) same-key?)))
+	(if subtable
+	    (let ((record (assoc-if key-2 (cdr subtable) same-key?)))
+	      (if record
+		  (set-cdr! record value)
+		  (set-cdr! subtable (cons (cons key-2 value) (cdr subtable)))))
+	    (set-cdr! local-table (cons (list key-1 (cons key-2 value)) (cdr local-table)))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+	    ((eq? m 'insert-proc!) insert!)
+	    (else (error "Unknown operation: TABLE" m))))
+    dispatch))
+    
+    
+	      
+	      
