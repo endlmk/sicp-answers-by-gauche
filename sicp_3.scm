007feb83ab98826a5ae30ff02f2c1e645a174407
endlmk <1269467+endlmk@users.noreply.github.com>
2022-01-28 23:26:39 +0900

    Done 3.31.

--- sicp_3.scm
+++ sicp_3.scm
@@ -705,3 +705,53 @@
 ;; full-adder 2*half + or
 ;; ripple-carry-adder n*full = 2*n*max(or*2+inv, and+or)+or
   
+;;ex3.31
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures ()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+	  (begin (set! signal-value new-value)
+		 (call-each action-procedures))
+	  'done))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons porc action-procedures))
+      (proc))
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+	    ((eq? m 'set-signal!) set-my-signal!)
+	    ((eq? m 'add-action!) accept-action-procedure!)
+	    (else (error "Unknown opeartion: WIRE" m))))
+    dispatch))
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin ((car procedures))
+	     (call-each (cdr procedures)))))
+(define (get-signal wire) (wire 'get-signal))
+(define (set-signal! wire new-value) ((wire 'set-signal!) new-value))
+(define (add-action! wire action-procedure) ((wire 'add-action!) action-procedure))
+
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda)) action the-agenda))
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+	(first-item)
+	(remove-first-agenda-item! the-agenda)
+	(propagate))))
+
+(define (probe name wire)
+  (add-action! wire
+	       (lambda ()
+		 (newline)
+		 (display name) (display " ")
+		 (display (current-time the-agenda))
+		 (display " New-value = ") (display (get-signal wire)))))
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+;;初期化がないと、after-delayが呼び出されず、アクションのagendaへの登録がされない。
