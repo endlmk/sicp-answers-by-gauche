f4a7ab5ff195c848b09013e3f7c475c963d8fd17
endlmk <1269467+endlmk@users.noreply.github.com>
2022-02-19 23:20:29 +0900

    Done 3.50.

--- sicp_3.scm
+++ sicp_3.scm
@@ -1143,9 +1143,37 @@
 	((serializer1 (serializer2 exchange)) account1 account2))))
 ;;ex3.49
 ;;事前にロックすべき口座が全てわかっていない場合、適切なロック順序とならず、デッドロックが起こりうる。
-			    
-	       
-	    
-		  
-	    
+
+(define the-empty-stream ())
+(define (stream-null? s) (null? s)) 
+(define-syntax cons-stream
+  (syntax-rules ()
+    [(_ a d) (cons a (delay d))]))
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+(define (stream-map porc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s)) (stream-map proc (stream-cdr s)))))
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+	     (stream-for-each proc (stream-cdr s)))))
+(define (display-stream s)
+  (stream-for-each display-line s))
+(define (display-line x) (newline) (display x))
+
+;;ex3.50
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map (cons proc (map stream-cdr argstreams))))))
+			  
 
