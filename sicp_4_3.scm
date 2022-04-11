9db668a094eabffe2aa1b7a15e7bf15c34edd4ee
endlmk <1269467+endlmk@users.noreply.github.com>
2022-04-11 22:57:02 +0900

    Done 4.36.

--- sicp_4_3.scm
+++ sicp_4_3.scm
@@ -2,3 +2,13 @@
 (define (an-integer-between l h)
   (require (<= l h))
   (amb l (an-integer-between (+ l 1) h)))
+
+;;ex4.36
+;;kに上限がないとバックトラックしないため、kが無限に増え続ける
+;;長辺kを決めて、他の二辺の和がkを超えないようにする
+(define (a-pytagorean-triple)
+  (let ((k (an-integer-starting-from 2)))
+    (let ((i (an-integer-between 1 k)))
+      (let ((j (an-integer-between i k)))
+	(require (= (+ (* i i) (* j j) (* k k))))
+	(list i j k)))))
