88a4f07e0b0869ff09f424cedeb8aff41b5616f2
endlmk <1269467+endlmk@users.noreply.github.com>
2022-04-17 22:20:04 +0900

    Done 4.38, 4.39.

--- sicp_4_3.scm
+++ sicp_4_3.scm
@@ -33,3 +33,67 @@
 (define (an-integer-between l h)
   (require (> h l))
   (amb l (an-integer-between (+ l 1) h)))
+(define (distinct? items)
+  (cond ((null? items) true)
+	((null? (cdr items)) true)
+	((member (car items) (cdr items)) false)
+	(else (distinct? (cdr items)))))
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4 5))
+	(cooper (amb 1 2 3 4 5))
+	(fletcher (amb 1 2 3 4 5))
+	(miller (amb 1 2 3 4 5))
+	(smith (amb 1 2 3 4 5)))
+    (require (distinct? (list baker cooper fletcher miller smith)))
+    (require (not (= baker 5)))
+    (require (not (= cooper 1)))
+    (require (not (= fletcher 5)))
+    (require (not (= fletcher 1)))
+    (require (> miller cooper))
+    (require (not (= (abs (- smith fletcher)) 1)))
+    (require (not (= (abs (- fletcher cooper)) 1)))
+    (list (list 'baker baker)
+	  (list 'cooper cooper)
+	  (list 'fletcher fletcher)
+	  (list 'miller miller)
+	  (list 'smith smith))))
+
+;;ex4.38
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4 5))
+	(cooper (amb 1 2 3 4 5))
+	(fletcher (amb 1 2 3 4 5))
+	(miller (amb 1 2 3 4 5))
+	(smith (amb 1 2 3 4 5)))
+    (require (distinct? (list baker cooper fletcher miller smith)))
+    (require (not (= baker 5)))
+    (require (not (= cooper 1)))
+    (require (not (= fletcher 5)))
+    (require (not (= fletcher 1)))
+    (require (> miller cooper))
+    ;;(require (not (= (abs (- smith fletcher)) 1)))
+    (require (not (= (abs (- fletcher cooper)) 1)))
+    (list (list 'baker baker)
+	  (list 'cooper cooper)
+	  (list 'fletcher fletcher)
+	  (list 'miller miller)
+	  (list 'smith smith))))
+;;答えは以下の5つ
+((baker 1) (cooper 2) (fletcher 4) (miller 3) (smith 5))
+((baker 1) (cooper 2) (fletcher 4) (miller 5) (smith 3))
+((baker 1) (cooper 4) (fletcher 2) (miller 5) (smith 3))
+((baker 3) (cooper 2) (fletcher 4) (miller 5) (smith 1))
+((baker 3) (cooper 4) (fletcher 2) (miller 5) (smith 1))
+　
+;;ex4.39
+;;制約の順序は解に影響しない。解の候補に対し、制約の順序によらず全ての制約を満たすことがチェックされるため。
+;;失敗する場合が多い制約を先にするほうが速い。そうでないと、深さ優先で探索して、より後の制約で失敗することが多くなりバックトラックが多くなるため。
+;;すなわち以下の順序。
+;;(require (distinct? (list baker cooper fletcher miller smith))) ;;3005 = 5^5 - 5*4*3*2*1
+;;(require (> miller cooper)) ;;1875 =  5^3*(1+2+3+4+5)
+;;(require (not (= (abs (- smith fletcher)) 1))) ;;1000 = 5^3*(1+2+2+2+1)
+;;(require (not (= (abs (- fletcher cooper)) 1))) ;;1000 = 5^3*(1+2+2+2+1)
+;;(require (not (= baker 5))) ;;625 = 5^4
+;;(require (not (= cooper 1))) ;;625 = 5^4
+;;(require (not (= fletcher 5))) ;;625 = 5^4
+;;(require (not (= fletcher 1))) ;;625 = 5^4
