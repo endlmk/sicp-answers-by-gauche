5f9098e350cc0c6b250eb8b9cafa70478ad1e346
endlmk <1269467+endlmk@users.noreply.github.com>
2022-05-02 22:13:22 +0900

    Done 4.56.

--- sicp_4_4.scm
+++ sicp_4_4.scm
@@ -5,3 +5,15 @@
 (job ?x (accounting . ?type))
 ;;3
 (address ?x (Slumerville . ?type))
+
+;;ex4.56
+;;1
+(and (supervisor ?person (Ben Bitdiddle))
+     (address ?person ?where)
+;;2
+(and (salary (Ben Bitdiddle) ?amount-ben)
+     (salary ?person ?amount)
+     (lisp-value < ?amount ?amount-ben))
+;;3
+(and (not (job ?boss (conputer . ?type)))
+     (supervisor ?person ?boss))
