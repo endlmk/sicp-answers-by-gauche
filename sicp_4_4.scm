27ca0cb8cc390caa68d86085fd86f1796697653d
endlmk <1269467+endlmk@users.noreply.github.com>
2022-05-05 14:17:56 +0900

    Done 4.61 to 4.63.

--- sicp_4_4.scm
+++ sicp_4_4.scm
@@ -57,3 +57,26 @@
 (and (lives-near ?person1 ?person2)
      (lisp-value compare ?person1 ?person2))
 
+;;ex4.61
+(?x next-to ?y in (1 (2 3) 4))
+(1 next-to (2 3) in (1 (2 3) 4))
+((2 3) next-to 4 in (1 (2 3) 4))
+(?x next-to 1 in (2 1 3 1))
+(2 next-to 1 in (2 1 3 1))
+(3 next-to 1 in (2 1 3 1))
+
+;;ex4.62
+(rule (last-pair (?x. ()) (?x))
+(rule (last-pair (?x . ?v))
+      (last-pair ?v))
+;;(last-pair ?x (3))に対しては(last-pair (3) (3))しか返らない。
+;;最後が3で他が任意の要素であるリストが条件を満たすが、クエリからは(3)しか得られない。
+
+;;ex4.63
+(rule (grand-child ?p ?c)
+     (and (son-of-parent ?p ?p1)
+          (son-of-parent ?p1 ?c)))
+(rule (son-of-parent ?p ?c)
+     (or (son ?p ?c)
+         (and (wife ?p ?w)
+              (son ?w ?c))))
