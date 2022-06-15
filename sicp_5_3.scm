9639916eea771ddfb3e2045bf59ba8cac47c39b8
endlmk <1269467+endlmk@users.noreply.github.com>
2022-06-15 22:51:06 +0900

    Done 5.22.

--- sicp_5_3.scm
+++ sicp_5_3.scm
@@ -90,5 +90,57 @@
 (define x (cons (list 1 2) (list 3 4)))
 (set-register-contents! count-leaves-iter-machine 'tree (list x x))
 (start count-leaves-iter-machine)
-(get-register-contents count-leaves-iter-machine 'val)	     
+(get-register-contents count-leaves-iter-machine 'val)    
 	     
+;;ex5.22
+(define append-machine
+  (make-machine
+   '(x y val tmp continue)
+   (list (list 'null? null?) (list 'cons cons) (list 'car car) (list 'cdr cdr))
+   '(controller
+     (assign continue (label done))
+     loop
+     (test (op null?) (reg x))
+     (branch (label base))
+     (save continue)
+     (assign continue (label rec))
+     (save x)
+     (assign x (op cdr) (reg x))
+     (goto (label loop))
+     rec
+     (restore x)
+     (restore continue)
+     (assign tmp (op car) (reg x))
+     (assign val (op cons) (reg tmp) (reg val))
+     (goto (reg continue))
+     base
+     (assign val (reg y))
+     (goto (reg continue))
+     done)))
+(set-register-contents! append-machine 'x '(1 2 3))
+(set-register-contents! append-machine 'y '(4 5 6))
+(start append-machine)
+(get-register-contents append-machine 'val)
+
+(define append!-machine
+  (make-machine
+   '(x y tmp)
+   (list (list 'null? null?) (list 'cdr cdr) (list 'set-cdr! set-cdr!))
+   '(controller
+     (save x)
+     loop
+     (assign tmp (op cdr) (reg x))
+     (test (op null?) (reg tmp))
+     (branch (label lastfound))
+     (assign x (op cdr) (reg x))
+     (goto (label loop))
+     lastfound
+     (assign x (op set-cdr!) (reg x) (reg y))
+     done
+     (restore x))))
+(set-register-contents! append!-machine 'x (list 1 2 3)) ;; '()形式のリストはimmutableなのでset-cdr!できない!
+(set-register-contents! append!-machine 'y (list 4 5 6))
+(start append!-machine)
+(get-register-contents append!-machine 'x)
+
+
