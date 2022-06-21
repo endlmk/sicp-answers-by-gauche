2605bbf7482fa146e94863ab3388a154491b2296
endlmk <1269467+endlmk@users.noreply.github.com>
2022-06-21 21:53:58 +0900

    Done 5,24,

--- sicp_5_4.scm
+++ sicp_5_4.scm
@@ -11,3 +11,35 @@ ev-cond
 ev-let
 (assign exp (op let->combination) (reg exp))
 (goto (label eval-dispatch))
+
+;;ex5.24
+ev-cond
+(assign unev (op cond-clauses) (reg exp))
+(save continue)
+ev-cond-loop
+(test (op null?) (reg unev))
+(branch (label ev-cond-null))
+(assign exp (op car) (reg unev))
+(test (op cond-else-clause?) (reg exp))
+(branch (label ev-cond-action))
+(save exp)
+(assign exp (op cond-predicate) (reg exp))
+(save unev)
+(save env)
+(assign continue (label ev-cond-decide))
+(goto (label eval-dispatch))
+ev-cond-decide
+(restore env)
+(restore unev)
+(restore exp)
+(test (op true?) (reg val))
+(branch (label ev-cond-action))
+(assign unev (op cdr) (reg unev))
+(goto (label ev-cond-loop))
+ev-cond-null
+(restore continue)
+(assign val (const #f))
+(goto (reg continue))		
+ec-cond-action
+(assign unev (op cond-action) (reg exp))
+(goto (label ev-sequence))
