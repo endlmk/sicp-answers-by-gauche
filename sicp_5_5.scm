c852624ee604189d1e81455fad0328e21f655f73
endlmk <1269467+endlmk@users.noreply.github.com>
2022-07-06 22:33:32 +0900

    Done 5.38 abc.

--- sicp_5_5.scm
+++ sicp_5_5.scm
@@ -528,3 +528,87 @@ after-lambda19
   (assign val (const ok))
   (restore continue)
 #<undef>
+
+;;ex5.38
+;;a
+(define (spread-arguments operands)
+  (let ((compile1 (compile (car operands) 'arg1 'next))
+	(compile2 (compile (cadr operands) 'arg2 'next)))
+    (list compile1 compile2)))
+
+;;b
+(define (compile exp target linkage)
+  (cond ((self-evaluating? exp) (compile-self-evaluating exp target linkage))
+	((quoted? exp) (compile-quoted exp target linkage))
+	((variable? exp) (compile-variable exp target linkage))
+	((assignment? exp) (compile-assignment exp target linkage))
+	((definition? exp) (compile-definition exp target linkage))
+	((if? exp) (compile-if exp target linkage))
+	((lambda? exp) (compile-lambda exp target linkage))
+	((begin? exp) (compile-sequence exp target linkage))
+	((cond? exp) (compile (cond->if exp) target linkage))
+	((memq (operator exp) '(= * - +)) (compile-opencode exp target linkage)) ;;add
+	((application? exp) (compile-application exp target linkage))
+	(else (error "unknown expression type: COMPILE" exp))))
+
+(define (compile-opencode exp target linkage)
+  (if (= (length exp) 3)
+      (let ((proc (operator exp))
+	    (args (spread-arguments (operands exp))))
+	(end-with-linkage linkage
+			  (append-instruction-sequences
+			   (car args)
+			   (preserving '(arg1)
+				       (cadr args)
+				       (make-instruction-sequence '(arg1 arg2)
+								  (list target)
+								  `((assign ,target (op ,proc) (reg arg1) (reg arg2))))))))
+      (error "Oepn code must have 2 operands: COMPILE")))
+      
+;;c
+;;open code
+(env)
+(val)
+  (assign val (op make-compiled-procedure) (label entry1) (reg env))
+  (goto (label after-lambda2))
+entry1
+  (assign env (op compiled-procedure-env) (reg proc))
+  (assign env (op extend-environment) (const (n)) (reg argl) (reg env))
+  (assign arg1 (op lookup-variable-value) (const n) (reg env))
+  (assign arg2 (const 1))
+  (assign val (op =) (reg arg1) (reg arg2))
+  (test (op false?) (reg val))
+  (branch (label false-branch4))
+true-branch3
+  (assign val (const 1))
+  (goto (reg continue))
+false-branch4
+  (save continue)
+  (assign proc (op lookup-variable-value) (const factorial) (reg env))
+  (assign arg1 (op lookup-variable-value) (const n) (reg env))
+  (assign arg2 (const 1))
+  (assign val (op -) (reg arg1) (reg arg2))
+  (assign argl (op list) (reg val))
+  (test (op primitive-procedure?) (reg proc))
+  (branch (label primitive-branch6))
+compiled-branch7
+  (assign continue (label proc-return9))
+  (assign val (op compiled-prcedure-entry) (reg proc))
+  (goto (reg val))
+proc-return9
+  (assign arg1 (reg val))
+  (goto (reg val))
+primitive-branch6
+  (assign arg1 (op apply-primitive-procedure) (reg proc) (reg argl))
+after-call8
+  (assign arg2 (op lookup-variable-value) (const n) (reg env))
+  (assign val (op *) (reg arg1) (reg arg2))
+  (restore continue)
+  (goto (reg continue))
+after-if5
+after-lambda2
+  (perform (op define-variable!) (const factorial) (reg val) (reg env))
+  (assign val (const ok))
+#<undef>
+
+
